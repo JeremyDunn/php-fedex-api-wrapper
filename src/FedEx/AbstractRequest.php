7209b9d282f96be5b77082db2ec2e96e0cfc90ab
Jeremy Dunn <innerspacesuit@gmail.com>
2012-08-03 11:52:50 -0500

    regenerated phpDocumentor docs

--- src/FedEx/AbstractRequest.php
+++ src/FedEx/AbstractRequest.php
@@ -1,6 +1,12 @@
 <?php
 namespace FedEx;
 
+/**
+ * Abstract class for Request classes
+ *
+ * @author      Jeremy Dunn <jeremy@jsdunn.info>
+ * @package     PHP FedEx API wrapper
+ */
 abstract class AbstractRequest
 {
     
