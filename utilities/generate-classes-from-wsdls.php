411ceb853c688efd5e6d259ce1d8484bcb150e8d
Colin O'Dell <colinodell@gmail.com>
2015-03-16 12:25:07 -0400

    Removed unused refereces

--- utilities/generate-classes-from-wsdls.php
+++ utilities/generate-classes-from-wsdls.php
@@ -4,13 +4,7 @@
 $vendorDir = dirname(__DIR__) . '/vendor';
 require_once $vendorDir . '/autoload.php';
 
-use     FedEx\Utility\Cli\GenerateCode,
-        Symfony\Component\Console\Input\InputArgument,
-        Symfony\Component\Console\Input\InputOption,
-        Symfony\Component\Console,
-        Symfony\Component\Console\Input\InputInterface,
-        Symfony\Component\Console\Output\OutputInterface,
-        Symfony\Component\Console\Application;
+use FedEx\Utility\Cli\GenerateCode;
 
 $app = new GenerateCode\Application();
 $app->run();
