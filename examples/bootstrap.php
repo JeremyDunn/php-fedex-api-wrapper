351b321cebb5dff31b5cdcc1646a872f1b7d7c3f
Jeremy Dunn <innerspacesuit@gmail.com>
2019-01-01 15:30:56 -0600

    update bootstrap.php

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 $vendorDir = dirname(__DIR__) . '/vendor';
 require_once $vendorDir . '/autoload.php';
+
+\FedEx\RequestFactory::$accountNumber = FEDEX_ACCOUNT_NUMBER;
+\FedEx\RequestFactory::$meterNumber = FEDEX_METER_NUMBER;
+\FedEx\RequestFactory::$key = FEDEX_KEY;
+\FedEx\RequestFactory::$password = FEDEX_PASSWORD;
